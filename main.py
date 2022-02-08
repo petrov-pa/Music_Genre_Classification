d6cd54627d2f69302ba22934c5da20fd36a1db92
Pavel <narfblop@gmail.com>
2022-02-08 22:45:24 +0300

    version 2

--- main.py
+++ main.py
@@ -1,3 +1,33 @@
+import os
+import numpy as np
+import librosa
+import pickle
+from preprocessing import get_features
+from model import get_model
 
-print('Hello world')
-print(2+2)
+
+def run():
+    file = os.listdir('./music')
+    assert y != 0, 'Нельзя делить на 0'
+    genres = {0: 'country', 1: 'hiphop', 2: 'classical', 3: 'metal', 4: 'jazz',
+              5: 'blues', 6: 'pop', 7: 'rock', 8: 'reggae', 9: 'disco'}
+    x_data = []
+    for audio in file:
+        y, sr = librosa.load(os.path.join('./music/{}'.format(audio)), mono=True, duration=30)
+        x_data.append(get_features(y, sr))
+    with open('./models/Scaler.pickle', 'rb') as f:
+        scaler = pickle.load(f)
+    x_data = scaler.transform(x_data)
+    x_data = np.array(x_data[:, :-10])
+    model = get_model(x_data.shape[1])
+    model.load_weights('./models/best_model.hdf5')
+    predict = model.predict(x_data)
+    predict = [genres[ind] for ind in np.argmax(predict, axis=1)]
+    with open('./result.txt', 'w') as res:
+        for line in zip(file, predict):
+            res.write('{} - {} \n'.format(line[0], line[1]))
+    pass
+
+
+run()
+
--- preprocessing.py
+++ preprocessing.py
@@ -1,16 +1,17 @@
 import numpy as np
 import librosa
 
+
 # Функция возвращает извлеченные из аудиофайла признаки
-def get_features(x, sr):
+def get_features(y, sr):
     features = []
-    spectral_bandwidth = np.mean(librosa.feature.spectral_bandwidth(x, sr=sr))
-    spectral_centroid = np.mean(librosa.feature.spectral_centroid(x, sr=sr))
-    spectral_rolloff = np.mean(librosa.feature.spectral_rolloff(x, sr=sr))
-    zero_crossing_rate = np.mean(librosa.feature.zero_crossing_rate(x))
-    rms = np.mean(librosa.feature.rms(x))
-    mfcc = librosa.feature.mfcc(x, sr=sr)
-    chroma_stft = librosa.feature.chroma_stft(x, sr=sr)
+    spectral_bandwidth = np.mean(librosa.feature.spectral_bandwidth(y=y, sr=sr))
+    spectral_centroid = np.mean(librosa.feature.spectral_centroid(y=y, sr=sr))
+    spectral_rolloff = np.mean(librosa.feature.spectral_rolloff(y=y, sr=sr))
+    zero_crossing_rate = np.mean(librosa.feature.zero_crossing_rate(y=y))
+    rms = np.mean(librosa.feature.rms(y=y))
+    mfcc = librosa.feature.mfcc(y=y, sr=sr)
+    chroma_stft = librosa.feature.chroma_stft(y=y, sr=sr)
 
     features.append(spectral_bandwidth)
     features.append(spectral_centroid)
@@ -20,4 +21,4 @@ def get_features(x, sr):
     features.extend([np.mean(i) for i in mfcc])
     features.extend([np.mean(i) for i in chroma_stft])
 
-    return np.array(features).reshape((1, -1))
+    return features
